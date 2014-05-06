79702de8444f2a5c16acc168f326fea07222d3ae
bergthor13 <bergthor13@ru.is>
2014-05-06 10:22:55 +0000

    First commit

---
+++ Skermstafir/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Skermstafir.Startup))]
+namespace Skermstafir
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
