9e948fb37740516d4ec419db85bd56da047ab2ce
gardyna <dagur13@ru.is>
2014-05-07 12:09:23 +0000

    dummy code in subtitle repo

--- Skermstafir/Repositories/SubtitleRepository.cs
+++ Skermstafir/Repositories/SubtitleRepository.cs
@@ -8,12 +8,18 @@ namespace Skermstafir.Repositories
 {
     public class SubtitleRepository
     {
-        // gets top "howMany" subtitles by newest
-        public List<SubtitleModel> GetSubtitleByNewest(int howMany)
+        // Add a subtitlemodel object to database
+        public void AddSubtitle(SubtitleModel model)
+        {
+
+        }
+
+        // queries database to get the newest starting from start and ending at end bot ionclusive
+        public List<SubtitleModel> GetSubtitleByNewest(int start, int end)
         {
             List<SubtitleModel> modelList = new List<SubtitleModel>();
             // here is dummy code for integration purposes
-            for (int i = 0; i < howMany; i++)
+            for (int i = 0; i < end - start; i++)
             {
                 SubtitleModel dummy = new SubtitleModel();
                 dummy.name = "Anchorman 2, The Legend Continues";
@@ -29,11 +35,12 @@ namespace Skermstafir.Repositories
             return modelList;
         }
 
-        public List<SubtitleModel> GetSubtitleByMostPopular(int howMany)
+        // queries database to get a list of most popular subtitles starting at index start and ending at index end both inclusive
+        public List<SubtitleModel> GetSubtitleByMostPopular(int start, int end)
         {
             List<SubtitleModel> modelList = new List<SubtitleModel>();
             // here is dummy code for integration purposes
-            for (int i = 0; i < howMany; i++)
+            for (int i = 0; i < end - start; i++)
             {
                 SubtitleModel dummy = new SubtitleModel();
                 dummy.name = "Anchorman 2, The Legend Continues";
@@ -46,6 +53,34 @@ namespace Skermstafir.Repositories
                 dummy.dateCreated = DateTime.Now;
                 modelList.Add(dummy);
             }
+            return modelList;
+        }
+
+        // query database to get all subtitles created by a specific user starting at index start and ending at index end both inclusive
+        public List<SubtitleModel> GetSubtitleByUser(String username, int start, int end)
+        {
+            List<SubtitleModel> modelList = new List<SubtitleModel>();
+            return modelList;
+        }
+
+        // query database to get a specific subtitle
+        public SubtitleModel GetSubtitleByID(int id)
+        {
+            SubtitleModel model = new SubtitleModel();
+            return model;
+        }
+
+        // query database to get subtitles by language starting at index start and ending at index end both inclusive
+        public List<SubtitleModel> GetSubtitleByLanguage(string Language, int start, int end)
+        {
+            List<SubtitleModel> modelList = new List<SubtitleModel>();
+            return modelList;
+        }
+
+        // query database to get subtitles by creation year starting from start and ending with end both inclusive
+        public List<SubtitleModel> GetSubtitleByCreationDate(int startYear, int endYear, int start, int end)
+        {
+            List<SubtitleModel> modelList = new List<SubtitleModel>();
             return modelList;
         }
     }
