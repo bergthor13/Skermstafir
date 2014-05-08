6486b44f0a91532a3416d2f84cbd480305e6914b
aegir13 <agirmar92@gmail.com>
2014-05-08 15:41:34 +0000

    Tiny changes.

--- Skermstafir/Models/AccountViewModels.cs
+++ Skermstafir/Models/AccountViewModels.cs
@@ -5,7 +5,7 @@ namespace Skermstafir.Models
     public class ExternalLoginConfirmationViewModel
     {
         [Required]
-        [Display(Name = "User name")]
+        [Display(Name = "Notendanafn")]
         public string UserName { get; set; }
     }
 
@@ -31,33 +31,33 @@ namespace Skermstafir.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "User name")]
+        [Display(Name = "Notendanafn")]
         public string UserName { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Lykilorð")]
         public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
+        [Display(Name = "Muna eftir mér?")]
         public bool RememberMe { get; set; }
     }
 
     public class RegisterViewModel
     {
         [Required]
-        [Display(Name = "User name")]
+        [Display(Name = "Notendanafn")]
         public string UserName { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "Lykilorðið verður að vera a.m.k. {2} stafir að lengd.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Lykilorð")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "Staðfesta lykilorð")]
+        [Compare("Password", ErrorMessage = "Þetta er ekki sama lykilorð og þú skrifaðir fyrir ofan, reyndu aftur.")]
         public string ConfirmPassword { get; set; }
     }
 }
