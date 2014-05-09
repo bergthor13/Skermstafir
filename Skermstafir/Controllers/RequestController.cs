31180f06ec706a4badfb09db0c7753ddaa295cff
aegir13 <agirmar92@gmail.com>
2014-05-09 14:18:09 +0000

    Eitthvad fkn rugl skilruh

--- Skermstafir/Controllers/RequestController.cs
+++ Skermstafir/Controllers/RequestController.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using Skermstafir.Models;
 using Skermstafir.Repositories;
 using System;
@@ -25,30 +24,4 @@ namespace Skermstafir.Controllers
             return View();
         }
 	}
-=======
-﻿using Skermstafir.Models;
-using Skermstafir.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace Skermstafir.Controllers
-{
-    public class RequestController : Controller
-    {
-        //
-        // GET: /Request/
-		public ActionResult ShowRequest(int? requestID)
-		{
-			return View();
-		}
-		// Adds a new request.
-        public ActionResult AddRequest()
-        {
-            return View();
-        }
-	}
->>>>>>> 4be52bc1ef7c2d7fdb466599853d340e2f213a40
 }
