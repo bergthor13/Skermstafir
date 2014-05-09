f768ec124d6c1b506d4704e3501cc5c5373f88cb
gardyna <dagur13@ru.is>
2014-05-09 11:53:31 +0000

    model changes

--- Skermstafir/Models/SubtitleModelList.cs
+++ Skermstafir/Models/SubtitleModelList.cs
@@ -7,15 +7,6 @@ namespace Skermstafir.Models
 {
     public class SubtitleModelList
     {
-        public List<SubtitleModel> modelList { get; set; }
-
-        public SubtitleModelList()
-        {
-            modelList = new List<SubtitleModel>();
-        }
-
-        public void Add(SubtitleModel model){
-            modelList.Add(model);
-        }
+        public List<Subtitle> modelList { get; set; }
     }
 }
