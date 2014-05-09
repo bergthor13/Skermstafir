219b7f1f6027493bbf1dac57c548a7143490c932
gardyna <dagur13@ru.is>
2014-05-09 15:49:10 +0000

    req repo update

--- Skermstafir/Repositories/RequestRepository.cs
+++ Skermstafir/Repositories/RequestRepository.cs
@@ -11,35 +11,29 @@ namespace Skermstafir.Repositories
         // adds new request to database
         public void AddRequest(RequestModel newRequest)
         {
-            using(SkermData db = new SkermData())
-            {
-                db.Requests.Add(newRequest.request);
-                db.SaveChanges();
-            }
+            SkermData db = new SkermData();
+            db.Requests.Add(newRequest.request);
+            db.SaveChanges();
         }
 
         //deletes a request with a specific id
         public void DeleteRequest(int id)
         {
-            using(SkermData db = new SkermData())
-            {
-                Request discardRequest = (from req in db.Requests
-                                          where req.IdRequest == id
-                                          select req).Single();
+            SkermData db = new SkermData();
+            Request discardRequest = (from req in db.Requests
+                                      where req.IdRequest == id
+                                      select req).Single();
                 db.Requests.Remove(discardRequest);
-            }
         }
 
         // queries and gets requests starting from index start and ending at index end
         public RequestModelList GetRequestByNewest(int start, int end)
         {
             RequestModelList model = new RequestModelList();
-            using (SkermData db = new SkermData())
-            {
-                model.modelList = (from req in db.Requests
-                                   orderby req.DateAdded
-                                   select req).Skip(start).Take(end - start).ToList();
-            }
+            SkermData db = new SkermData();
+            model.modelList = (from req in db.Requests
+                               orderby req.DateAdded
+                               select req).Skip(start).Take(end - start).ToList();
             return model;
         }
 
@@ -47,12 +41,10 @@ namespace Skermstafir.Repositories
         public RequestModelList GetByMostPopular(int start, int end)
         {
             RequestModelList model = new RequestModelList();
-            using (SkermData db = new SkermData())
-            {
-                model.modelList = (from req in db.Requests
-                                   orderby req.Votes.Count
-                                   select req).Skip(start).Take(end - start).ToList();
-            }
+            SkermData db = new SkermData();
+            model.modelList = (from req in db.Requests
+                               orderby req.Votes.Count
+                               select req).Skip(start).Take(end - start).ToList();
             return model;
         }
 
@@ -60,13 +52,11 @@ namespace Skermstafir.Repositories
         public RequestModelList GetRequestByUser(String username, int start, int end)
         {
             RequestModelList model = new RequestModelList();
-            using (SkermData db = new SkermData())
-            {
-                model.modelList = (from req in db.Requests
-                                   where req.Username == username
-                                   orderby req.IdRequest
-                                   select req).Skip(start).Take(end - start).ToList();
-            }
+            SkermData db = new SkermData();
+            model.modelList = (from req in db.Requests
+                                where req.Username == username
+                                orderby req.IdRequest
+                                select req).Skip(start).Take(end - start).ToList();
             return model;
         }
 
@@ -74,26 +64,21 @@ namespace Skermstafir.Repositories
         public RequestModel GetRequestByID(int id)
         {
             RequestModel model = new RequestModel();
-            using (SkermData db = new SkermData())
-            {
-                model.request = (from req in db.Requests
-                                 where req.IdRequest == id
-                                 select req).Single();
-                model.votes = model.request.Votes.Count;
-            }
+            SkermData db = new SkermData();
+            model.request = (from req in db.Requests
+                                where req.IdRequest == id
+                                select req).Single();
             return model;
         }
         // queries database and gets request in language starting at index start and ending at index end both inclusive
         public RequestModelList GetRequestByLanguage(String language, int start, int end)
         {
             RequestModelList modelList = new RequestModelList();
-            using (SkermData db = new SkermData())
-            {
-                modelList.modelList = (from req in db.Requests
-                                       where req.Language.Name == language
-                                       orderby req.IdRequest
-                                       select req).Skip(start).Take(end - start).ToList();
-            }
+            SkermData db = new SkermData();
+            modelList.modelList = (from req in db.Requests
+                                    where req.Language.Name == language
+                                    orderby req.IdRequest
+                                    select req).Skip(start).Take(end - start).ToList();
             return modelList;
         }
     }
