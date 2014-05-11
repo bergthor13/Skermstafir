a41ff67a578ff817c77d4ea767dbbbcc39ecae5f
gardyna <dagur13@ru.is>
2014-05-11 15:25:51 +0000

    updated test class definitions

--- Skermstafir.Tests/Controllers/ModelTests.cs
+++ Skermstafir.Tests/Controllers/ModelTests.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Web.Mvc;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Skermstafir;
+using Skermstafir.Controllers;
 
 namespace Skermstafir.Tests.Controllers
 {
+	[TestClass]
     class ModelTests
     {
     }
