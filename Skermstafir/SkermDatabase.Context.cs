b32065162a68e3388092f160022453b9f004cc38
gardyna <dagur13@ru.is>
2014-05-12 11:49:36 +0000

    databaseconnection update

--- Skermstafir/SkermDatabase.Context.cs
+++ Skermstafir/SkermDatabase.Context.cs
@@ -16,7 +16,7 @@ namespace Skermstafir
     public partial class SkermData : DbContext
     {
         public SkermData()
-            : base("name=SkermData")
+            : base("name=SkermDatabase")
         {
         }
     
