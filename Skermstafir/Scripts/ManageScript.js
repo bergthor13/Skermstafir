751547fe3604540bdf72b0428a3b7f5d26714e67
aegir13 <agirmar92@gmail.com>
2014-05-13 10:11:22 +0000

    javascript og eh rugl

---
+++ Skermstafir/Scripts/ManageScript.js
@@ -0,0 +1,13 @@
+﻿alert("before");
+$(document).ready(function () {
+    alert("in between");
+    function SubDelete() {
+        if (confirm('Ertu viss um að þú viljir eyða þessum skjátexta?')) {
+            alert("true");
+        }
+        else {
+            return;
+        }
+    }
+});
+alert("after");
