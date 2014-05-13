2de354481a1c99d37eaa6fecb6f64161fe9f0cfa
aegir13 <agirmar92@gmail.com>
2014-05-13 10:13:12 +0000

    laga bögg

--- Skermstafir/Scripts/ManageScript.js
+++ Skermstafir/Scripts/ManageScript.js
@@ -1,13 +1,1 @@
-﻿alert("before");
-$(document).ready(function () {
-    alert("in between");
-    function SubDelete() {
-        if (confirm('Ertu viss um að þú viljir eyða þessum skjátexta?')) {
-            alert("true");
-        }
-        else {
-            return;
-        }
-    }
-});
-alert("after");
+﻿
