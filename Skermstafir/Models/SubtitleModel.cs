43677c5e9c9972419ae73c859b596ec9e8089a6d
bergthor13 <bergthor13@ru.is>
2014-05-13 00:29:58 +0000

    Added a comment

--- Skermstafir/Models/SubtitleModel.cs
+++ Skermstafir/Models/SubtitleModel.cs
@@ -8,6 +8,7 @@ namespace Skermstafir.Models
     public class SubtitleModel
     {
         public Subtitle subtitle { get; set; }
+		// 8 is count of genres.
 		public bool[] genreValue = new bool[8];
 		public string actorsForView;
 		public SubtitleModel()
