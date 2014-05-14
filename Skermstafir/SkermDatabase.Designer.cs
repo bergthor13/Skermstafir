35e26a2fbf834eddbcfd24f41c6722ca356ba99f
gardyna <dagur13@ru.is>
2014-05-14 12:40:19 +0000

    DATABASE 6.0 the reckonig

--- Skermstafir/SkermDatabase.Designer.cs
+++ Skermstafir/SkermDatabase.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Dagur\Documents\GitHub\Skermstafir\Skermstafir\SkermData.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Dagur\Documents\GitHub\Skermstafir\Skermstafir\SkermDatabase.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
