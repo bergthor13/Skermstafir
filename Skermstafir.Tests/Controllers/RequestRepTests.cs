357efc6d5af23bc662dbc75fb3b71ffaedee8996
gardyna <dagur13@ru.is>
2014-05-15 15:32:49 +0000

    unit testing

--- Skermstafir.Tests/Controllers/RequestRepTests.cs
+++ Skermstafir.Tests/Controllers/RequestRepTests.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Skermstafir;
-using Skermstafir.Controllers;
+using Skermstafir.Repositories;
 using Skermstafir.Models;
 
 namespace Skermstafir.Tests.Controllers {
@@ -37,8 +37,15 @@ namespace Skermstafir.Tests.Controllers {
         public void GetRequestByNewest()
         {
 			// Arrange
+			Skermstafir.SkermData db = new Skermstafir.SkermData();
+			RequestRepository reqRep = new RequestRepository(db);
 			// Act
+			var result = reqRep.GetRequestByNewest(0, 5);
 			// Assert
+			Assert.IsNotNull(result.modelList);
+			for (int i = 0; i < result.modelList.Count - 1; i++) {
+				Assert.IsTrue(result.modelList[i].DateAdded >= result.modelList[i + 1].DateAdded);
+			}
         }
 
         // queries database and gets most popular requests starting at index start and ending at index end
@@ -46,8 +53,15 @@ namespace Skermstafir.Tests.Controllers {
         public void GetByMostPopular()
         {
 			// Arrange
+			Skermstafir.SkermData db = new Skermstafir.SkermData();
+			RequestRepository reqRep = new RequestRepository(db);
 			// Act
+			var result = reqRep.GetByMostPopular(0, 5);
 			// Assert
+			Assert.IsNotNull(result.modelList);
+			for (int i = 0; i < result.modelList.Count - 1; i++) {
+				Assert.IsTrue(result.modelList[i].Votes.Count >= result.modelList[i + 1].Votes.Count);
+			}
         }
 
         // queries database and gets all requests from a specific user starting at index start and ending at index end both inclusive
@@ -55,8 +69,16 @@ namespace Skermstafir.Tests.Controllers {
         public void GetRequestsByUser()
         {
 			// Arrange
+			Skermstafir.SkermData db = new Skermstafir.SkermData();
+			RequestRepository reqRep = new RequestRepository(db);
+			String user = "Elvar";
 			// Act
+			var result = reqRep.GetRequestsByUser(user);
 			// Assert
+			Assert.IsNotNull(result.modelList);
+			foreach (var item in result.modelList) {
+				Assert.IsTrue(item.Username == user);
+			}
         }
 
         // queries and gets a request by id
@@ -64,8 +86,13 @@ namespace Skermstafir.Tests.Controllers {
         public void GetRequestByID()
         {
 			// Arrange
+			Skermstafir.SkermData db = new Skermstafir.SkermData();
+			RequestRepository reqRep = new RequestRepository(db);
+			int id = 1;
 			// Act
+			var result = reqRep.GetRequestByID(id);
 			// Assert
+			Assert.IsNotNull(result);
         }
 
         // queries database and gets request in language starting at index start and ending at index end both inclusive
@@ -73,38 +100,77 @@ namespace Skermstafir.Tests.Controllers {
         public void GetRequestByLanguage()
         {
 			// Arrange
+			Skermstafir.SkermData db = new Skermstafir.SkermData();
+			RequestRepository reqRep = new RequestRepository(db);
+			String lang = "Íslenska";
 			// Act
+			var result = reqRep.GetRequestByLanguage(lang, 0, 5);
 			// Assert
+			Assert.IsNotNull(result.modelList);
+			foreach (var item in result.modelList) {
+				Assert.IsTrue(item.Language.Name == lang);
+			}
         }
 
 		[TestMethod]
 		public void GetRequestsByString() {
 			// Arrange
+			Skermstafir.SkermData db = new Skermstafir.SkermData();
+			RequestRepository reqRep = new RequestRepository(db);
+			String name = "s";
 			// Act
+			var result = reqRep.GetRequestsByString(name);
 			// Assert
+			Assert.IsNotNull(result.modelList);
+			foreach (var item in result.modelList) {
+				Assert.IsTrue(item.Name.Contains(name));
+			}
 		}
 
 		[TestMethod]
 		public void GetRequestsByYear() {
 			// Arrange
+			Skermstafir.SkermData db = new Skermstafir.SkermData();
+			RequestRepository reqRep = new RequestRepository(db);
+			int start = 1990;
+			int end = 2000;
 			// Act
+			var result = reqRep.GetRequestsByYear(start, end);
 			// Assert
+			Assert.IsNotNull(result.modelList);
+			foreach (var item in result.modelList) {
+				Assert.IsTrue(item.YearCreated >= start && item.YearCreated <= end);
+			}
 		}
 
 		[TestMethod]
 		public void GetRequestsByGenre() {
 			// Arrange
+			Skermstafir.SkermData db = new Skermstafir.SkermData();
+			RequestRepository reqRep = new RequestRepository(db);
+			SearchRepository search = new SearchRepository(db);
+			Skermstafir.Genre genre = search.GetGenreByID(2);
 			// Act
+			var result = reqRep.GetRequestsByGenre(genre.Name);
 			// Assert
+			Assert.IsNotNull(result.modelList);
+			foreach (var item in result.modelList) {
+				Assert.IsTrue(item.Genres.Contains(genre));
+			}
 		}
 
 		[TestMethod]
 		public void VoteContainsRequest()
 		{
 			// Arrange
+			Skermstafir.SkermData db = new Skermstafir.SkermData();
+			RequestRepository reqRep = new RequestRepository(db);
+			Skermstafir.Request req = reqRep.GetRequestByID(2).request;
+			Skermstafir.Vote vote = reqRep.GetVoteByUserID("35d6a087-7485-4517-8a23-fbed72ef60d4");
 			// Act
+			var result = reqRep.VoteContainsRequest(vote, req);
 			// Assert
-			// Rollback
+			Assert.IsNotNull(result);
 		}
 
 		[TestMethod]
@@ -129,8 +195,14 @@ namespace Skermstafir.Tests.Controllers {
 		public void GetVoteByUserID()
 		{
 			// Arrange
+			Skermstafir.SkermData db = new Skermstafir.SkermData();
+			RequestRepository reqRep = new RequestRepository(db);
+			String user = "35d6a087-7485-4517-8a23-fbed72ef60d4";
 			// Act
+			var result = reqRep.GetVoteByUserID(user);
 			// Assert
+			Assert.IsNotNull(result);
+			Assert.IsTrue(result.UserId == user);
 		}
 	}
 }
